9f43f87380f91ee10487e1c46d48bf000cc72cfc
Takeshi Kondo <ncukondo@gmail.com>
2024-04-01 15:57:19 +0900

    add post

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,13 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function (req: VercelRequest, res: VercelResponse) {
-  const { name = "World" } = req.query;
-  res.send(`Hello ${name}!`);
+  if (req.method === 'POST') {
+    // POSTリクエストを処理します
+    const { name = "World" } = req.body;
+    res.send(`Post:Hello ${name}!`);
+  } else {
+    // その他のHTTPメソッドを処理します
+    const { name = "World" } = req.query;
+    res.send(`Get:Hello ${name}!`);
+  }
 }
