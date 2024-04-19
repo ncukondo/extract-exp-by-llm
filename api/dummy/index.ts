936a95c3339e6d4f240108fdc53bf4151158386d
Takeshi Kondo <ncukondo@gmail.com>
2024-04-19 11:55:31 +0900

    fix verce.json

--- api/dummy/index.ts
+++ api/dummy/index.ts
@@ -1,8 +1,11 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { dummyHandler } from "../../services/make-response.js";
 
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 export default async function (req: VercelRequest, res: VercelResponse) {
   if (req.method === 'POST') {
+    await sleep(20000);
     res.status(200).json(await dummyHandler(req.body));
   } else {
     res.send(`The method ${req.method} is not supported`);
--- sample/sample_post.sh
+++ sample/sample_post.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # JSONファイルのパス
-FILE_PATH="./sample/sample_long.json"
+FILE_PATH="./sample/sample.json"
 
 # true end point: https://extract-exp-by-llm.vercel.app/api/
 
 # APIエンドポイント
-API_ENDPOINT="https://extract-exp-by-llm.vercel.app/api/"
+API_ENDPOINT="http://localhost:3000/api/dummy"
 
 # curlを使用してファイルの内容をPOSTする
 curl -o ./sample/output.json -X POST -H "Content-Type: application/json" -d @$FILE_PATH $API_ENDPOINT  > ./sample/output.log  2> ./sample/error.log
